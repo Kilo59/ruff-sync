791a3c332432f0fc8d1bfdfd8faf0c9d69c4dabf
Gabriel <gabriel59kg@gmail.com>
2024-03-31 19:29:47 -0400

    Fix dotted-keys & table updates (#5)

--- ruff_sync.py
+++ ruff_sync.py
@@ -1,22 +1,76 @@
 from __future__ import annotations
 
 import asyncio
+import logging
 import pathlib
+import warnings
 from argparse import ArgumentParser
+from functools import lru_cache
 from io import StringIO
-from typing import TYPE_CHECKING, Final, NamedTuple
+from pprint import pformat as pf
+from typing import TYPE_CHECKING, Final, Literal, NamedTuple, overload
 
 import httpx
 import tomlkit
 from httpx import URL
+from tomlkit import TOMLDocument, table
+from tomlkit import key as toml_key
+from tomlkit.container import OutOfOrderTableProxy
+from tomlkit.exceptions import TOMLKitError
+from tomlkit.items import Table
+from tomlkit.toml_file import TOMLFile
 
 if TYPE_CHECKING:
-    from collections.abc import Iterable
+    from collections.abc import Iterable, Mapping
 
 __version__ = "0.0.1.dev0"
 
+_DEFAULT_EXCLUDE: Final[set[str]] = {"per-file-ignores"}
+
+LOGGER = logging.getLogger(__name__)
+
+
+class Arguments(NamedTuple):
+    upstream: URL
+    source: pathlib.Path
+    exclude: Iterable[str]
+
+    @classmethod
+    @lru_cache(maxsize=1)
+    def fields(cls) -> set[str]:
+        return set(cls._fields)
+
+
+# NOTE: this is not used
+@lru_cache(maxsize=1)
+def get_config(
+    source: pathlib.Path,
+) -> Mapping[Literal["upstream", "source", "exclude"], str | list[str]]:
+    local_toml = source / "pyproject.toml"
+    # TODO: use pydantic to validate the toml file
+    cfg_result = {}
+    if local_toml.exists():
+        toml = tomlkit.parse(local_toml.read_text())
+        config = toml.get("tool", {}).get("ruff-sync")
+        if config:
+            for arg, value in config.items():
+                if arg in Arguments.fields():
+                    cfg_result[arg] = value
+                else:
+                    warnings.warn(f"Unknown ruff-sync configuration: {arg}", stacklevel=2)
+    return cfg_result
+
+
+@lru_cache(maxsize=1)
+def _resolve_source(source: str | pathlib.Path) -> pathlib.Path:
+    if isinstance(source, str):
+        source = pathlib.Path(source)
+    return source.resolve(strict=True)
+
 
 def _get_cli_parser() -> ArgumentParser:
+    # TODO: determine if args was provided by user or not
+    # https://docs.python.org/3/library/argparse.html#nargs
     parser = ArgumentParser()
     parser.add_argument(
         "upstream",
@@ -27,15 +81,17 @@ def _get_cli_parser() -> ArgumentParser:
         "--source",
         type=pathlib.Path,
         default=".",
-        help="The directory to sync the pyproject.toml file to.",
+        help="The directory to sync the pyproject.toml file to. Default: .",
         required=False,
     )
+    parser.add_argument(
+        "--exclude",
+        nargs="+",
+        help=f"Exclude certain ruff configs. Default: {' '.join(_DEFAULT_EXCLUDE)}",
+        type=set,
+        default=_DEFAULT_EXCLUDE,
+    )
     return parser
-
-
-class Arguments(NamedTuple):
-    upstream: URL
-    source: pathlib.Path
 
 
 async def download(url: URL, client: httpx.AsyncClient) -> StringIO:
@@ -45,21 +101,109 @@ async def download(url: URL, client: httpx.AsyncClient) -> StringIO:
     return StringIO(response.text)
 
 
-def toml_ruff_parse(
-    toml_s: str, exclude: Iterable[str] = ("per-file-ignores",)
-) -> tomlkit.TOMLDocument:
-    """Parse a TOML string for the tool.ruff section excluding certain ruff configs."""
-    ruff_toml: tomlkit.TOMLDocument = tomlkit.parse(toml_s)["tool"]["ruff"]  # type: ignore[index,assignment]
+@overload
+def get_ruff_tool_table(
+    toml: str | TOMLDocument,
+    create_if_missing: Literal[True] = ...,
+    exclude: Iterable[str] = ...,
+) -> Table: ...
+
+
+@overload
+def get_ruff_tool_table(
+    toml: str | TOMLDocument,
+    create_if_missing: Literal[False] = ...,
+    exclude: Iterable[str] = ...,
+) -> Table | None: ...
+
+
+def get_ruff_tool_table(
+    toml: str | TOMLDocument, create_if_missing: bool = True, exclude: Iterable[str] = ()
+) -> Table | None:
+    """
+    Get the tool.ruff section from a TOML string.
+    If it does not exist, create it.
+    """
+    if isinstance(toml, str):
+        doc: TOMLDocument = tomlkit.parse(toml)
+    else:
+        doc = toml
+    try:
+        tool: Table = doc["tool"]  # type: ignore[assignment]
+        ruff = tool["ruff"]
+        LOGGER.debug("Found `tool.ruff` section.")
+    except KeyError:
+        if not create_if_missing:
+            return None
+        LOGGER.info("No `tool.ruff` section found, creating it.")
+        tool = table(True)
+        ruff = table()
+        tool.append("ruff", ruff)
+        doc.append("tool", tool)
+    if not isinstance(ruff, Table):
+        raise TypeError(f"Expected table, got {type(ruff)}")
     for section in exclude:
+        if section in ruff:
+            LOGGER.info(f"Exluding section `lint.{section}` from ruff config.")
+            ruff.pop(section)
+    return ruff
+
+
+def toml_ruff_parse(toml_s: str, exclude: Iterable[str]) -> TOMLDocument:
+    """Parse a TOML string for the tool.ruff section excluding certain ruff configs."""
+    ruff_toml: TOMLDocument = tomlkit.parse(toml_s)["tool"]["ruff"]  # type: ignore[index,assignment]
+    for section in exclude:
+        LOGGER.info(f"Exluding section `lint.{section}` from ruff config.")
         ruff_toml["lint"].pop(section, None)  # type: ignore[union-attr]
     return ruff_toml
 
 
 def merge_ruff_toml(
-    source: tomlkit.TOMLDocument, upstream_update: tomlkit.TOMLDocument
-) -> tomlkit.TOMLDocument:
-    """Merge the source and upstream ruff toml config."""
-    source["tool"]["ruff"].update(upstream_update)  # type: ignore[index,union-attr]
+    source: TOMLDocument, upstream_ruff_doc: TOMLDocument | Table | None
+) -> TOMLDocument:
+    """
+    Merge the source and upstream tool ruff config
+    """
+    source_tool_ruff = get_ruff_tool_table(source)
+    if upstream_ruff_doc:
+        source_tool_ruff.update(upstream_ruff_doc)
+        out_of_order: dict[str, OutOfOrderTableProxy] = {}
+
+        # TODO: simplify this
+
+        # fix out of order tables
+        for key, value in upstream_ruff_doc.items():
+            dotted_components = key.split(".")
+            if len(dotted_components) > 1:
+                LOGGER.info(f"Found dot-noted key: {key}")
+            if isinstance(value, OutOfOrderTableProxy):
+                out_of_order[key] = value
+                LOGGER.debug(f"Found out of order table: {key}")
+                for nested_key, nested_value in value.items():
+                    dotted_key = toml_key([key, nested_key])
+                    LOGGER.debug(f"Nested: {dotted_key} - {type(nested_value)}")
+                    if isinstance(nested_value, Table):
+                        LOGGER.debug(f"Nested {dotted_key} {type(nested_value).__name__}")
+        LOGGER.debug(f"Out of order tables:\n{list(out_of_order.keys())}")
+        LOGGER.debug(f"Out of order:\n{pf(list(out_of_order.values()))}")
+        # remove out of order tables
+        for key in out_of_order:
+            LOGGER.debug(f"Removing out of order table: {key}")
+            popped: OutOfOrderTableProxy = source_tool_ruff.pop(key)
+            # now breakup the table and add it back in the correct order
+            for nested_key, nested_value in popped.items():
+                dotted_key = toml_key([key, nested_key])
+                LOGGER.debug(f"Adding back: {dotted_key}")
+                # TODO: isinstance check rather than try/except
+                try:
+                    source_tool_ruff[dotted_key] = nested_value
+                except TOMLKitError as e:
+                    LOGGER.debug(f"Error adding {dotted_key}: {e}")
+                    table = {k: v for k, v in popped.items() if k == nested_key}
+                    LOGGER.info(f"Adding Table: {pf(table)}")
+                    source_tool_ruff.append(key, table)
+    else:
+        LOGGER.warning("No upstream ruff config section found.")
     return source
 
 
@@ -69,21 +213,25 @@ async def sync(
     """Sync the upstream pyproject.toml file to the source directory."""
     print("Syncing Ruff...")
     if args.source.is_file():
-        source_toml_path = args.source
+        _source_toml_path = args.source
     else:
-        source_toml_path = args.source / "pyproject.toml"
-    source_toml_path = source_toml_path.resolve(strict=True)
+        _source_toml_path = args.source / "pyproject.toml"
+    source_toml_file = TOMLFile(_source_toml_path.resolve(strict=True))
 
+    # NOTE: there's no particular reason to use async here.
     async with httpx.AsyncClient() as client:
         file_buffer = await download(args.upstream, client)
+        LOGGER.info(f"Downloaded upstream file from {args.upstream}")
 
-    ruff_toml = toml_ruff_parse(file_buffer.read())
-    merged_toml = merge_ruff_toml(
-        tomlkit.parse(source_toml_path.read_text()),
-        ruff_toml,
+    upstream_ruff_toml = get_ruff_tool_table(
+        file_buffer.read(), create_if_missing=False, exclude=args.exclude
     )
-    source_toml_path.write_text(merged_toml.as_string())
-    print(f"Updated {source_toml_path.relative_to(pathlib.Path.cwd())}")
+    merged_toml = merge_ruff_toml(
+        source_toml_file.read(),
+        upstream_ruff_toml,
+    )
+    source_toml_file.write(merged_toml)
+    print(f"Updated {_source_toml_path.relative_to(pathlib.Path.cwd())}")
 
 
 PARSER: Final[ArgumentParser] = _get_cli_parser()
@@ -91,11 +239,13 @@ PARSER: Final[ArgumentParser] = _get_cli_parser()
 
 def main() -> None:
     args = PARSER.parse_args()
+    # config = get_config(args.source)
     asyncio.run(
         sync(
             Arguments(
                 upstream=args.upstream,
                 source=args.source,
+                exclude=args.exclude,
             )
         )
     )
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,20 +1,55 @@
-import pathlib
-from collections.abc import Generator
-from pprint import pformat as pf
-from typing import Final
+from __future__ import annotations
 
+import contextlib
+import os
+import pathlib
+import shutil
+from pprint import pformat as pf
+from typing import TYPE_CHECKING, Final
+
+import httpx
 import pytest
 import respx
 import tomlkit
 from httpx import URL
-from pyfakefs.fake_filesystem import FakeFilesystem
+from pytest import param
 from tomlkit import TOMLDocument
+from tomlkit.items import Table
+from tomlkit.toml_file import TOMLFile
 
 import ruff_sync
 
-PROJECT_ROOT: Final = pathlib.Path(__file__).parent.parent
-ROOT_PYPROJECT_TOML: Final = PROJECT_ROOT / "pyproject.toml"
-assert ROOT_PYPROJECT_TOML.exists(), f"{ROOT_PYPROJECT_TOML} does not exist"
+if TYPE_CHECKING:
+    from collections.abc import Generator, Sequence
+
+    from _pytest.mark.structures import ParameterSet
+    from pyfakefs.fake_filesystem import FakeFilesystem
+
+TEST_ROOT: Final = pathlib.Path(__file__).parent
+PROJECT_ROOT: Final = TEST_ROOT.parent
+ROOT_PYPROJECT_TOML_PATH: Final = PROJECT_ROOT / "pyproject.toml"
+ROOT_PYPROJECT_TOML: Final = TOMLFile(ROOT_PYPROJECT_TOML_PATH)
+
+SAMPLE_TOML_W_RUFF_SYNC_CFG: Final = TEST_ROOT / "w_ruff_sync_cfg"
+SAMPLE_TOML_WITHOUT_RUFF_SYNC_CFG: Final = TEST_ROOT / "wo_ruff_sync_cfg"
+SAMPLE_TOML_WITHOUT_RUFF_CFG: Final = TEST_ROOT / "wo_ruff_cfg"
+
+TERMINAL_SIZE: Final[int] = shutil.get_terminal_size().columns
+
+
+TOML_STRS_PARAMS: Final[Sequence[ParameterSet]] = [
+    param(t.joinpath("pyproject.toml").read_text(), id=t.name)
+    for t in (
+        SAMPLE_TOML_W_RUFF_SYNC_CFG,
+        SAMPLE_TOML_WITHOUT_RUFF_SYNC_CFG,
+        SAMPLE_TOML_WITHOUT_RUFF_CFG,
+    )
+]
+
+
+@pytest.fixture(scope="session")
+def sep_str() -> str:
+    return f"{'*' * TERMINAL_SIZE}"
 
 
 @pytest.fixture(scope="session")
@@ -23,8 +58,8 @@ def pyproject_toml_s() -> str:
     The contents of the root pyproject.toml file.
     Prevents problems with the file or file system being modified during tests.
     """
-    s = ROOT_PYPROJECT_TOML.read_text()
-    assert s, f"{ROOT_PYPROJECT_TOML} was empty"
+    s = ROOT_PYPROJECT_TOML_PATH.read_text()
+    assert s, f"{ROOT_PYPROJECT_TOML_PATH} was empty"
     return s
 
 
@@ -35,19 +70,18 @@ def test_ruff_sync():
 @pytest.fixture
 def toml_s() -> str:
     """A sample pyproject.toml file with ruff config."""
-    return """
-    [tool.ruff.lint]
-    target-version = "py38"
-    line-length = 120
-    lint.select = ["F", "ASYNC"]
-    lint.ignore = ["W191", "E111"]
+    return """[tool.ruff.lint]
+target-version = "py38"
+line-length = 120
+lint.select = ["F", "ASYNC"]
+lint.ignore = ["W191", "E111"]
 
-    [tool.ruff.lint.per-file-ignores]
-    "__init__.py" = [
-        "F401", # unused import
-        "F403", # star imports
-    ]
-    """
+[tool.ruff.lint.per-file-ignores]
+"__init__.py" = [
+    "F401", # unused import
+    "F403", # star imports
+]
+"""
 
 
 @pytest.mark.parametrize(
@@ -75,6 +109,47 @@ def test_toml_ruff_parse(toml_s: str, exclude: tuple[str, ...]):
         assert section in lint_config, f"{section} was incorrectly excluded"
 
 
+@pytest.mark.parametrize("toml_str", TOML_STRS_PARAMS)
+def test_get_ruff_tool_table(toml_str: str):
+    """
+    Test the get_ruff_tool_table() returns a tool.ruff table regardless of if the
+    input has one or not.
+    """
+    ruff_table = ruff_sync.get_ruff_tool_table(toml_str)
+    print(type(ruff_table))
+    print(f"Ruff Table:\n{ruff_table.as_string()}")
+    assert isinstance(ruff_table, Table)
+
+
+@pytest.mark.parametrize(
+    "source",
+    [
+        param(
+            SAMPLE_TOML_WITHOUT_RUFF_CFG.joinpath("pyproject.toml").read_text(),
+            id="no ruff cfg",
+        ),
+        param(
+            SAMPLE_TOML_WITHOUT_RUFF_SYNC_CFG.joinpath("pyproject.toml").read_text(),
+            id="no sync cfg",
+        ),
+    ],
+)
+def test_merge_ruff_toml(source: str, toml_s: str, sep_str: str):
+    upstream_toml: str = toml_s
+    print(f"Source\n{sep_str}\n{source}\n")
+    print(f"Upstream\n{sep_str}\n{upstream_toml}")
+
+    source_toml = tomlkit.parse(source)
+    upstream_ruff: Table = tomlkit.parse(upstream_toml)["tool"]["ruff"]  # type: ignore[index,assignment]
+
+    merged_ruff = ruff_sync.merge_ruff_toml(source_toml, upstream_ruff_doc=upstream_ruff)
+    print(f"Merged\n{sep_str}\n{merged_ruff.as_string()}\n")
+
+    source_ruff = source_toml.get("tool", {}).get("ruff")
+    for key, _ in upstream_ruff.items():
+        assert key in source_ruff, f"{key} was not in the updated source ruff config"
+
+
 @pytest.fixture
 def mock_http(toml_s: str) -> Generator[respx.MockRouter, None, None]:
     with respx.mock(base_url="https://example.com/") as respx_mock:
@@ -95,10 +170,90 @@ def fake_fs_source(fs: FakeFilesystem, pyproject_toml_s: str) -> pathlib.Path:
     return ff_path
 
 
+@pytest.mark.xfail
 @pytest.mark.asyncio
-async def test_sync(mock_http: respx.MockRouter, fake_fs_source: pathlib.Path):
+async def test_sync_updates_ruff_config(
+    mock_http: respx.MockRouter, fake_fs_source: pathlib.Path, sep_str: str
+):
+    original_toml = fake_fs_source.read_text()
+    original_ruff_config: Table = tomlkit.parse(original_toml)["tool"]["ruff"]  # type: ignore[index,assignment]
+    print(f"Original tool.ruff:\n{sep_str}\n{tomlkit.dumps(original_ruff_config)}\n")
+
     upstream = URL("https://example.com/pyproject.toml")
-    await ruff_sync.sync(ruff_sync.Arguments(upstream=upstream, source=fake_fs_source))
+    upstream_toml = httpx.get(upstream).text  # noqa: ASYNC100 # blocking but doesn't matter
+    await ruff_sync.sync(
+        ruff_sync.Arguments(upstream=upstream, source=fake_fs_source, exclude=())
+    )
+    updated_toml = fake_fs_source.read_text()
+    updated_ruff_config: Table = tomlkit.parse(updated_toml)["tool"]["ruff"]  # type: ignore[index,assignment]
+    print(f"\nUpdated tool.ruff\n{sep_str}\n{tomlkit.dumps(updated_ruff_config)}")
+    assert original_toml != updated_toml
+
+    assert set(original_ruff_config.keys()).issubset(set(updated_ruff_config.keys()))
+
+    # Ensure the updated ruff config has the same keys as the original
+    for key in original_ruff_config.keys():
+        assert (
+            key in updated_ruff_config
+        ), f"Original key {key} was not in updated ruff config"
+
+    # Ensure the updated ruff config has the expected updated values from upstream
+    upstream_ruff_config: Table = tomlkit.parse(upstream_toml)["tool"]["ruff"]  # type: ignore[index,assignment]
+    print("Upstream updates...")
+    for key, value in upstream_ruff_config.items():
+        print(f"  {key}", end=" ")
+        assert updated_ruff_config[key] == value, f"{key} was not updated"
+        print("✅")
+
+
+@contextlib.contextmanager
+def temp_cd(path: pathlib.Path) -> Generator[pathlib.Path, None, None]:
+    """Context manager to temporarily change the working directory."""
+    old_dir = pathlib.Path.cwd()
+    os.chdir(path)
+    try:
+        yield pathlib.Path.cwd()
+    finally:
+        os.chdir(old_dir)
+
+
+@pytest.mark.filterwarnings("ignore:Unknown ruff-sync configuration")
+@pytest.mark.parametrize(
+    ["sample_toml_dir", "expected_config"],
+    [
+        param(
+            SAMPLE_TOML_W_RUFF_SYNC_CFG,
+            {
+                "upstream": "https://raw.githubusercontent.com/pydantic/pydantic/main/pyproject.toml",
+                "exclude": ["per-file-ignores", "ignore", "line-length"],
+            },
+            id=SAMPLE_TOML_W_RUFF_SYNC_CFG.name,
+        ),
+        param(
+            SAMPLE_TOML_WITHOUT_RUFF_CFG,
+            {
+                "upstream": "https://raw.githubusercontent.com/pydantic/pydantic/main/pyproject.toml",
+                "exclude": ["per-file-ignores", "ignore", "line-length"],
+            },
+            id=SAMPLE_TOML_WITHOUT_RUFF_CFG.name,
+        ),
+        param(
+            SAMPLE_TOML_WITHOUT_RUFF_SYNC_CFG,
+            {},
+            id=SAMPLE_TOML_WITHOUT_RUFF_SYNC_CFG.name,
+        ),
+    ],
+)
+def test_loading_ruff_sync_config(
+    sample_toml_dir: pathlib.Path, expected_config: dict[str, list[str] | str]
+):
+    """Test that that the ruff_sync settings are loaded from the pyproject.toml."""
+    sample_pyproject_toml = sample_toml_dir / "pyproject.toml"
+    assert sample_pyproject_toml.exists(), f"{sample_pyproject_toml} does not exist"
+
+    config = ruff_sync.get_config(sample_toml_dir)
+    print(f"Config:\n{pf(config)}")
+    assert expected_config == config
 
 
 if __name__ == "__main__":
