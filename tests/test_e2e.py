624cc804363fa17c209d734f08d156d316c8d62d
Gabriel Gore <gabriel59kg@gmail.com>
2024-04-13 13:14:59 -0400

    better failure diff

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -98,8 +98,12 @@ async def test_ruff_sync(prep_env):
 
     print(f"\nUpdated toml\n***************\n\n{prep_env.source_path.read_text()}")
 
-    assert expected_toml.unwrap() == actual_toml.unwrap()
-    assert prep_env.expected_toml == prep_env.source_path.read_text()
+    assert dict(tomlkit.parse(prep_env.expected_toml)) == dict(
+        tomlkit.parse(prep_env.source_path.read_text())
+    ), "TOML items do not match expected"
+    assert (
+        prep_env.expected_toml == prep_env.source_path.read_text()
+    ), "TOML items match but strings do not match exactly"
 
 
 if __name__ == "__main__":
