37f8073d405d5a83f01bf78d0d7fd8b016301d44
Gabriel Gore <gabriel59kg@gmail.com>
2024-04-14 19:45:05 -0400

    reduce complexity

--- ruff_sync.py
+++ ruff_sync.py
@@ -7,7 +7,6 @@ import warnings
 from argparse import ArgumentParser
 from functools import lru_cache
 from io import StringIO
-from pprint import pformat as pf
 from typing import TYPE_CHECKING, Any, Final, Literal, NamedTuple
 
 import httpx
@@ -27,6 +26,10 @@ __version__ = "0.0.1.dev0"
 _DEFAULT_EXCLUDE: Final[set[str]] = {"per-file-ignores"}
 
 LOGGER = logging.getLogger(__name__)
+
+
+class RuffSyncError(Exception):
+    pass
 
 
 class Arguments(NamedTuple):
@@ -120,48 +123,56 @@ async def download(url: URL, client: httpx.AsyncClient) -> StringIO:
     return StringIO(response.text)
 
 
+def _resolve_py_project_toml(path: pathlib.Path) -> pathlib.Path:
+    py_project_toml = path if path.is_file() else path / "pyproject.toml"
+    return py_project_toml.resolve(strict=True)
+
+
 async def sync(
     args: Arguments,
 ) -> None:
     """Sync the upstream pyproject.toml file to the source directory."""
-    print("Syncing Ruff...")
-    if args.source.is_file():
-        _source_toml_path = args.source
-    else:
-        _source_toml_path = args.source / "pyproject.toml"
-    source_toml_file = TOMLFile(_source_toml_path.resolve(strict=True))
+    print("🔄 Syncing Ruff ...")
+
+    py_project_toml_path = _resolve_py_project_toml(args.source)
+    source_toml_file = TOMLFile(_resolve_py_project_toml(py_project_toml_path))
 
     if args.exclude:
         print("Exluding:", args.exclude)
 
     # NOTE: there's no particular reason to use async here.
     async with httpx.AsyncClient() as client:
+        print(f"⬇️ Downloading upstream file from {args.upstream}")
         file_buffer = await download(args.upstream, client)
-        LOGGER.info(f"Downloaded upstream file from {args.upstream}")
 
     upstream_doc: TOMLDocument = tomlkit.parse(file_buffer.getvalue())
     upsteam_ruff: Table | None = upstream_doc.get("tool", {}).get("ruff")
     if not upsteam_ruff:
-        raise ValueError("No `tool.ruff` section found in upstream file.")
+        raise RuffSyncError("No `tool.ruff` section found in upstream file.")
 
     source_doc: TOMLDocument = source_toml_file.read()
-    source_tool: Table = source_doc["tool"]  # type: ignore[assignment]
+    for section, value in source_doc.items():
+        LOGGER.error(f"{section}: {type(value)}{value}")
+    source_tool: Table | OutOfOrderTableProxy = source_doc["tool"]  # type: ignore[assignment]
     source_ruff: Table | None = source_tool.get("ruff")  # type: ignore[assignment]
     if not source_ruff:
         source_ruff = table()
-        source_tool["ruff"] = source_ruff
+        if isinstance(source_tool, OutOfOrderTableProxy):
+            raise RuffSyncError(
+                f"tool section is {type(source_tool)} {list(source_tool.keys())}"
+                " and missing 'ruff' section."
+            )
+        source_tool.add("ruff", source_ruff)
 
     # iterate over the upstream ruff config and update the corresponding section in the
     # source ruff config unless it is in the exclude list
     # TODO: exlcude list
     simple_updates: dict[str | Key, Any] = {}
     for section, value in upsteam_ruff.items():
-        LOGGER.info(f"{section}: {type(value)}{value}")
         if not isinstance(value, (Table, OutOfOrderTableProxy)):
             simple_updates[section] = value
         else:
             for sub_section, sub_value in value.items():
-                LOGGER.info(f"{section}.{sub_section}: {type(sub_value)}{sub_value}")
                 if isinstance(sub_value, Table):
                     source_ruff.append(section, {sub_section: sub_value})
                     # newline after table
@@ -172,10 +183,9 @@ async def sync(
                     source_ruff.get(section, {}).pop(sub_section, None)
                     simple_updates[dotted_key] = sub_value
 
-    LOGGER.info(f"Update: ->\n{pf(simple_updates)}")
     source_ruff.update(simple_updates)
     source_toml_file.write(source_doc)
-    print(f"Updated {_source_toml_path.relative_to(pathlib.Path.cwd())}")
+    print(f"✅ Updated {py_project_toml_path.relative_to(pathlib.Path.cwd())}")
 
 
 PARSER: Final[ArgumentParser] = _get_cli_parser()
