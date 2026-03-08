626d3a2c2cab726c7477f5280818c56bb3c65451
Gabriel Gore <gabriel59kg@gmail.com>
2026-03-08 15:14:14 -0400

    bump version

--- ruff_sync.py
+++ ruff_sync.py
@@ -20,7 +20,7 @@ from tomlkit import TOMLDocument, table
 from tomlkit.items import Table
 from tomlkit.toml_file import TOMLFile
 
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 
 _DEFAULT_EXCLUDE: Final[set[str]] = {"lint.per-file-ignores"}
 _GITHUB_REPO_PATH_PARTS_COUNT: Final[int] = 2
