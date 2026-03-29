8e68a0821d96d302045077ff9400b702ca9799b1
Gabriel G <gabriel59kg@gmail.com>
2026-03-29 10:33:05 -0400

    MissingType

--- src/ruff_sync/constants.py
+++ src/ruff_sync/constants.py
@@ -2,6 +2,33 @@
 
 from __future__ import annotations
 
+import enum
 from typing import Final
 
+__all__: Final[list[str]] = [
+    "DEFAULT_EXCLUDE",
+    "MISSING",
+    "MissingType",
+]
+
 DEFAULT_EXCLUDE: Final[set[str]] = {"lint.per-file-ignores"}
+
+
+class MissingType(enum.Enum):
+    """Used to represent a missing value sentinel.
+
+    This can be used to properly type fields that use the `MissingType.SENTINEL` as a default.
+
+    Example:
+        >>> from ruff_sync.constants import MissingType, MISSING
+        >>> def foo(bar: int | None | MissingType = MISSING) -> None:
+        ...     if bar is MissingType.SENTINEL:
+        ...         print("bar is missing")
+        ...     else:
+        ...         print(f"bar is {bar}")
+    """
+
+    SENTINEL = enum.auto()
+
+
+MISSING: Final[MissingType] = MissingType.SENTINEL
